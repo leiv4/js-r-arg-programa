c0d6c4da3e5f70b645de6eb8ee09585f394209ca
Nicolás Leiva <nicolasleiva@proton.me>
2025-03-24 18:10:07 -0300

    Update nivel2.js

--- js/nivel2.js
+++ js/nivel2.js
@@ -311,9 +311,23 @@ let comidasFavoritas = ["asado", "pastel de papas", "hamburguesa", "pizza"];
 // NOTA: Es posible que desees utilizar el operador aritmético modulo (%):
 // Calcula el resto al dividir.
 //
-// 10% 3 = 1 - en 10 tenemos 3 * 3 + 1
-// 16% 4 = 0 - en 16 tenemos 4 * 4
-// 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
+// 10 % 3 = 1 - en 10 tenemos 3 * 3 + 1
+// 16 % 4 = 0 - en 16 tenemos 4 * 4
+// 19 % 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
+
+// let i;
+
+// for (i = 1; i <= 50; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
 //////////////////////////////////////////////////////////////////////////////
 // ¡Felicidades! ¡Has terminado el Nivel 2 de JavaScript Básico!            //
